e9628d02ed313dc33aeb9b0e3b450ca489b5a205
Subak.io <50165193+subakioinc@users.noreply.github.com>
2022-08-19 12:46:52 +0900

    Update MiddlewareToVcc.h

--- MiddlewareToVcc.h
+++ MiddlewareToVcc.h
@@ -1,4 +1,6 @@
-typedef struct _MiddlewareToVccNaviInfo {
+#include <stdint.h>
+
+typedef struct _NaviInfo {
     uint8_t nav_mode; //항법모드 (single/RTK/INS)
     float nav_roll; //degree roll
     float nav_pitch; //degree pitch
@@ -12,20 +14,27 @@ typedef struct _MiddlewareToVccNaviInfo {
     double nav_latitude; //degree 위도
     float nav_heave; //m Heave
     float nav_gpstime; // hhmmss.s GPS시간
-} MiddlewareToVccNaviInfo;
+} NaviInfo;
 
 
-typedef struct _MiddlewareToVccWeatherInfo {
+
+typedef struct _WeatherInfo {
     float wea_airtem; //degree 공기 온도
     float wea_wattem; //degree 해수 온도
     float wea_press;  //bar 기압
-    float wea_relhum  //% 상대습도
+    float wea_relhum; //% 상대습도
     float wea_dewpt;  //degree 이슬점
-    float wea_windirt //degree 풍향(절대)
+    float wea_windirt; //degree 풍향(절대)
     float wea_winspdt; //knots 풍속(절대)
     float wea_windirr;  //degree 풍향(상대)
     float wea_watspdr;  //knots 풍속(상대)
     float wea_watdir;   //degree 유향
     float wea_watspd;  //knots 유속
     float wea_visibiran; //m 가시거리
-} MiddlewareToVccWeatherInfo;
+} WeatherInfo;
+
+typedef struct _MiddlewareToVcc {
+    NaviInfo navInfo;
+    WeatherInfo weatherinfo;
+} MiddlewareToVcc;
+
