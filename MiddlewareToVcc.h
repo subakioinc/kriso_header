360d51427cbe535f61e12dd96e757f7c5a626f29
Subak.io <50165193+subakioinc@users.noreply.github.com>
2022-08-19 12:53:06 +0900

    Update MiddlewareToVcc.h

--- MiddlewareToVcc.h
+++ MiddlewareToVcc.h
@@ -1,4 +1,3 @@
-
 #include <stdint.h>
 
 typedef struct _NaviInfo {
@@ -55,17 +54,17 @@ typedef struct _MiddlewareToVccNaviInfo {
     double nav_latitude; //degree 위도
     float nav_heave; //m Heave
     float nav_gpstime; // hhmmss.s GPS시간
-	float wea_airtem; //degree 공기 온도
-	float wea_wattem; //degree 해수 온도
-	float wea_press;  //bar 기압
-	float wea_relhum  //% 상대습도
-	float wea_dewpt;  //degree 이슬점
-	float wea_windirt //degree 풍향(절대)
-	float wea_winspdt; //knots 풍속(절대)
-	float wea_windirr;  //degree 풍향(상대)
-	float wea_watspdr;  //knots 풍속(상대)
-	float wea_watdir;   //degree 유향
-	float wea_watspd;  //knots 유속
-	float wea_visibiran; //m 가시거리
+    float wea_airtem; //degree 공기 온도
+    float wea_wattem; //degree 해수 온도
+    float wea_press;  //bar 기압
+    float wea_relhum  //% 상대습도
+    float wea_dewpt;  //degree 이슬점
+    float wea_windirt //degree 풍향(절대)
+    float wea_winspdt; //knots 풍속(절대)
+    float wea_windirr;  //degree 풍향(상대)
+    float wea_watspdr;  //knots 풍속(상대)
+    float wea_watdir;   //degree 유향
+    float wea_watspd;  //knots 유속
+    float wea_visibiran; //m 가시거리
 } MiddlewareToVccNaviInfo;
 */
